572087a7529d70c77738bda48bb63fb546422b53
vandodev <vandoaparecido@hotmail.com>
2024-03-26 08:10:06 -0300

    :framed_picture: :robot: :framed_picture: Criando formulário de criação de conta

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,6 @@ import {createServerComponentClient} from "@supabase/auth-helpers-nextjs"
 import {cookies} from "next/headers"
 import { RedirectType, redirect } from "next/navigation"
 
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 
 import {
   Tabs,
@@ -20,6 +9,7 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs"
+import { CreateAccounrForm } from "@/components/auth/create-account"
 
 export default async function Home() {
   let loggedIn = false
@@ -54,50 +44,10 @@ export default async function Home() {
 
         </TabsList>
         <TabsContent value="create-account">
-          <Card>
-            <CardHeader>
-              <CardTitle>Create your account</CardTitle>
-              <CardDescription>
-                Create your account
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" defaultValue="Pedro Duarte" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="username">Username</Label>
-                <Input id="username" defaultValue="@peduarte" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Save changes</Button>
-            </CardFooter>
-          </Card>
+            <CreateAccounrForm />
         </TabsContent>
         <TabsContent value="login">
-          <Card>
-            <CardHeader>
-              <CardTitle>Login</CardTitle>
-              <CardDescription>
-                Change your password here. After saving, you'll be logged out.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="current">Current password</Label>
-                <Input id="current" type="password" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="new">New password</Label>
-                <Input id="new" type="password" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Save password</Button>
-            </CardFooter>
-          </Card>
+         
         </TabsContent>
       </Tabs>
     </div>
---
+++ components/auth/create-account.tsx
@@ -0,0 +1,85 @@
+"use client"
+import * as z from "zod"
+import {useForm} from "react-hook-form"
+
+import {zodResolver} from "@hookform/resolvers/zod"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+
+const formShema = z.object({
+    email: z.string({
+        required_error: "Email is required"
+    })
+    .email({
+        message: "Must be a valid email"
+    }),
+
+    password: z.string({
+        required_error: "Password shoul is required"
+    })
+    .min(7, {
+        message:"Password must have at least 7 characters"
+    })
+    .max(12)
+})
+
+export function CreateAccounrForm(){
+    const form = useForm<z.infer<typeof formShema>>({
+        resolver: zodResolver(formShema),
+        defaultValues:{
+            email:"",
+            password:""
+        }
+    })
+
+    const onSubmit = async(values: z.infer<typeof formShema>) => {
+        console.log(values)
+    }
+
+   return (
+        <div className="flex flex-col justify-center items-center space-y-2">
+            <span className="text-lg">You will love it</span>
+            <Form {...form}>
+                <form 
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="flex flex-col space-y-2"                
+                >
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="E-mail" {...field}/>
+                                </FormControl>
+                                <FormDescription>This is your E-mail</FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Password" {...field}/>
+                                </FormControl>
+                                <FormDescription>This is your Password</FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <Button type="submit" >Create account</Button>
+
+                </form>
+            </Form>
+        </div>
+   )
+}
