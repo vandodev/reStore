d74d43d356628ec958335cac8f456ea9901864a2
vandodev <vandoaparecido@hotmail.com>
2024-03-27 09:06:02 -0300

    :framed_picture: :robot: :framed_picture: Redirecionando usuário caso não esteje logado

--- app/user-app/page.tsx
+++ app/user-app/page.tsx
@@ -1,3 +1,20 @@
+import {createServerComponentClient} from "@supabase/auth-helpers-nextjs"
+import {cookies} from "next/headers"
+import { RedirectType, redirect } from "next/navigation"
+
 export default async function UserApp(){
+    let loggedIn = false
+
+    try {
+      const supabase = createServerComponentClient({cookies})
+      const {data: {session}} = await supabase.auth.getSession()
+  
+      if(session) loggedIn = true
+    } catch (error) {
+      console.log("Home", error)
+    }finally{
+      if(!loggedIn) redirect("/", RedirectType.replace)
+    }
+
     return <h1>Usuário logado</h1>
 }
