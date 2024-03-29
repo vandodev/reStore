059f4f449733449f61977f568db66368de15ca80
vandodev <vandoaparecido@hotmail.com>
2024-03-29 07:23:36 -0300

    :framed_picture: :robot: :framed_picture: Configurando tabList do shadcn

--- app/user-app/page.tsx
+++ app/user-app/page.tsx
@@ -1,9 +1,13 @@
 import { UserNav } from "@/components/common/user-nav"
 import UserAppHeader from "@/components/user-app/user-app-header"
 import { Sidebar } from "@/components/user-app/user-app-sidebar"
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
 import {createServerComponentClient} from "@supabase/auth-helpers-nextjs"
 import {cookies} from "next/headers"
 import { RedirectType, redirect } from "next/navigation"
+import {PlusCircleIcon} from "lucide-react"
+import {Button} from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
 
 export default async function UserApp(){
     let loggedIn = false
@@ -30,6 +34,73 @@ export default async function UserApp(){
                   <Sidebar className="hidden lg:block" />
                   <div className="col-span-3 lg:col-span-4 lg:border-l">
                     <div className="h-full px-4 py-6 lg:px-8">
+                      <Tabs defaultValue="photos" className="h-full space-y-6">
+                        <div className="space-between flex items-center">
+                          <TabsList>
+                            <TabsTrigger value="photos" className="relative">
+                              Photos
+                            </TabsTrigger>
+                            <TabsTrigger value="documents">Documents</TabsTrigger>
+                            <TabsTrigger value="other" disabled>
+                              Other
+                            </TabsTrigger>
+                          </TabsList>
+                          <div className="ml-auto mr-4">
+                            <Button>
+                              <PlusCircleIcon className="mr-2 h-4 w-4" />
+                              Add Collection
+                            </Button>
+                          </div>
+                        </div>
+                        <TabsContent
+                          value="photos"
+                          className="border-none p-0 outline-none"
+                        >
+                          <div className="flex items-center justify-between">
+                            <div className="space-y-1">
+                              <h2 className="text-2xl font-semibold tracking-tight">
+                                Photos collections
+                              </h2>
+                              <p className="text-sm text-muted-foreground">
+                                The photos you already enhancedy
+                              </p>
+                            </div>
+                          </div>
+                          <Separator className="my-4" />
+                          <div className="relative">
+                            Lista 1
+                          </div>
+                          <div className="mt-6 space-y-1">
+                            <h2 className="text-2xl font-semibold tracking-tight">
+                              Made for You
+                            </h2>
+                            <p className="text-sm text-muted-foreground">
+                              Your personal playlists. Updated daily.
+                            </p>
+                          </div>
+                          <Separator className="my-4" />
+                          <div className="relative">
+                            Lista 2                           
+                          </div>
+                        </TabsContent>
+                        <TabsContent
+                          value="documents"
+                          className="h-full flex-col border-none p-0 data-[state=active]:flex"
+                        >
+                          <div className="flex items-center justify-between">
+                            <div className="space-y-1">
+                              <h2 className="text-2xl font-semibold tracking-tight">
+                                New Episodes
+                              </h2>
+                              <p className="text-sm text-muted-foreground">
+                                Your favorite documents. Updated daily.
+                              </p>
+                            </div>
+                          </div>
+                          <Separator className="my-4" />
+                          {/* <PodcastEmptyPlaceholder /> */}
+                        </TabsContent>
+                      </Tabs>
                     </div>
                   </div>
                 </div>
