2ea8e088b6d1dc0cb8faec0272f5172caa4ce46a
vandodev <vandoaparecido@hotmail.com>
2024-03-31 07:31:19 -0300

    :framed_picture: :robot: :framed_picture: Protegendo rota

--- app/api/ai/replicate/route.ts
+++ app/api/ai/replicate/route.ts
@@ -1,13 +1,25 @@
 import { NextResponse, NextRequest } from "next/server";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
 interface NextRequestWithImage extends NextRequest {
   imageUrl: string;
 }
 
 export async function POST(req: NextRequestWithImage, res: NextResponse) {
-  console.log("POST reseived")
+  // console.log("POST reseived")
   const { imageUrl } = await req.json();
-  console.log(imageUrl)
+  // console.log(imageUrl)
+
+  const supabase = createRouteHandlerClient({ cookies });
+
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+
+  if (!session || error) new NextResponse("Login in order to restore image");
+
   return NextResponse.json({messge: "Teste"},{
     status:200
   } );
